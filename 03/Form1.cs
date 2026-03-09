92e4d1741ebb3a401967f930b0fdb8566fd8c477
Simcaa007 <havelkova.si@seznam.cz>
2026-03-09 21:32:56 +0100

    hotovo

---
+++ 03/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string t = textBox2.Text;
+
+            string[] slova = s.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (string slovo in slova)
+            {
+                char posledni = slovo[slovo.Length - 1];
+
+                if (t.Contains(posledni))
+                {
+                    listBox1.Items.Add(slovo);
+                }
+            }
+
+        }
+    }
+}
