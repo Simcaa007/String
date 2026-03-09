ea074eb129e9773f22e2fde8dba1d367268957e8
Simcaa007 <havelkova.si@seznam.cz>
2026-03-09 20:32:59 +0100

    hotovo

--- 01/Form1.Designer.cs
+++ 01/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 113);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "kok\r\ncopak\r\nnevim\r\n5ha\r\n67\r\nnegrrr\r\n!$";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(149, 173);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 01/Form1.cs
+++ 01/Form1.cs
@@ -16,5 +16,37 @@ namespace _01
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] retezce = new string[textBox1.Lines.Length];
+            int p = 0;
+            foreach (string s in textBox1.Lines)
+            {
+                if (s[0] >= 'a' && s[0] <= 'z')
+                {
+                    string slovo = char.ToUpper(s[0]) + s.Substring(1) + ".";
+                    slovo = slovo.Trim();
+                    retezce[p] = slovo;
+                }
+                else
+                {
+                    string slovo = s + ".";
+                    slovo = slovo.Trim();
+                    retezce[p] = slovo;
+                }
+                p++;
+            }
+
+            textBox1.Clear();
+
+            textBox1.Lines = retezce;
+        }
     }
 }
